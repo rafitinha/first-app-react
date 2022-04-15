3ea087f3c57d282142c3edebf42f34038e587286
Arrombado <rafaoc7232@hotmail.com>
2022-04-15 16:34:30 -0300

    feat(dashboard): :construction: create dashboard component

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,79 @@
 import React from 'react';
 
-import Title from './styles';
+import { FiChevronRight } from 'react-icons/fi';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 export const Dashboard: React.FC = () => {
-    return <Title>Dashboard</Title>;
+    return (
+        <>
+            <img src={logoImg} alt="Github Explorer" />
+            <Title>Explore repositórios no Github</Title>
+            <Form>
+                <input placeholder="Digite o nome do repositório" />
+                <button type="submit">Pesquisa</button>
+            </Form>
+            <Repositories>
+                <a href="teste">
+                    <img
+                        src="https://avatars.githubusercontent.com/u/11011662?v=4"
+                        alt="Rafael Cruz"
+                    />
+                    <div>
+                        <strong>rocketseat/unform</strong>
+                        <p>
+                            Easy peasy highly scalable ReactJS & React Native
+                            forms!
+                        </p>
+                    </div>
+                    <FiChevronRight size={20} />
+                </a>
+                <a href="teste">
+                    <img
+                        src="https://avatars.githubusercontent.com/u/11011662?v=4"
+                        alt="Rafael Cruz"
+                    />
+                    <div>
+                        <strong>rocketseat/unform</strong>
+                        <p>
+                            Easy peasy highly scalable ReactJS & React Native
+                            forms!
+                        </p>
+                    </div>
+                    <FiChevronRight size={20} />
+                </a>
+                <a href="teste">
+                    <img
+                        src="https://avatars.githubusercontent.com/u/11011662?v=4"
+                        alt="Rafael Cruz"
+                    />
+                    <div>
+                        <strong>rocketseat/unform</strong>
+                        <p>
+                            Easy peasy highly scalable ReactJS & React Native
+                            forms!
+                        </p>
+                    </div>
+                    <FiChevronRight size={20} />
+                </a>
+                <a href="teste">
+                    <img
+                        src="https://avatars.githubusercontent.com/u/11011662?v=4"
+                        alt="Rafael Cruz"
+                    />
+                    <div>
+                        <strong>rocketseat/unform</strong>
+                        <p>
+                            Easy peasy highly scalable ReactJS & React Native
+                            forms!
+                        </p>
+                    </div>
+                    <FiChevronRight size={20} />
+                </a>
+            </Repositories>
+        </>
+    );
 };
 
 export default Dashboard;
